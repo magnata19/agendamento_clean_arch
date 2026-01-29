abbfe82fb07ddfee5d3bf0dec9b33e45611d6f57
Davidson <dehpacifico@gmail.com>
2026-01-29 19:26:46 -0300

    initial structure from clean architecture.

---
+++ src/main/java/br/com/curso/agendamento_clean_arch/AgendamentoCleanArchApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.agendamento_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoCleanArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoCleanArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/curso/agendamento_clean_arch/AgendamentoCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.agendamento_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoCleanArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
